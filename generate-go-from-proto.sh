e156503435122972a90e5914c96b4943f871031c
psx95 <pranav.ps95@hotmail.com>
2024-07-21 23:49:15 -0400

    Update gitignore & protogen script

--- generate-go-from-proto.sh
+++ generate-go-from-proto.sh
@@ -2,6 +2,6 @@
 echo "Generating Go code from proto files"
 
 mkdir -p invoiceservice/pb
-protoc --proto_path=invoiceservice/proto/ invoiceservice/proto/*.proto --go-grpc_out=invoiceservice/pb   
+protoc --proto_path=invoiceservice/proto/ invoiceservice/proto/*.proto --go_out=invoiceservice/pb --go-grpc_out=invoiceservice/pb  
 
 echo "Code generated from proto files"
